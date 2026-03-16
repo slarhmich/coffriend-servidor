dce13ac7ac87377e72c0bd14edea3d8843845252
slarhmich <s.larhmich@gmail.com>
2026-03-16 18:46:33 +0100

    Create Spring project.

---
+++ coffriend-server/src/main/java/com/brewingcode/coffriend_server/CoffriendServerApplication.java
@@ -0,0 +1,13 @@
+package com.brewingcode.coffriend_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffriendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffriendServerApplication.class, args);
+	}
+
+}
---
+++ coffriend-server/src/test/java/com/brewingcode/coffriend_server/CoffriendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewingcode.coffriend_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffriendServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
