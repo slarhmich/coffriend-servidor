d9d94c3578b8186f02a89fd2bc81040816fd02bb
slarhmich <s.larhmich@gmail.com>
2026-04-16 11:50:16 +0200

    initial commit

---
+++ src/main/java/com/brewingcode/coffriend_servidor/CoffriendServidorApplication.java
@@ -0,0 +1,13 @@
+package com.brewingcode.coffriend_servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffriendServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffriendServidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewingcode/coffriend_servidor/CoffriendServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewingcode.coffriend_servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffriendServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
