5d7ba3223413b27557174f22f2b7622978d0afd4
slarhmich <s.larhmich@gmail.com>
2026-04-20 15:22:56 +0200

    fix: add missing PasswordEncoder bean to SecurityConfig

--- src/main/java/com/brewingcode/coffriend_servidor/security/SecurityConfig.java
+++ src/main/java/com/brewingcode/coffriend_servidor/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -34,5 +36,10 @@ public class SecurityConfig {
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
